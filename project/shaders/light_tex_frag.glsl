d987494ba988edab04551b22a888a78b44ad8fa1
SW234 <98165157+SW234@users.noreply.github.com>
2026-06-09 23:08:57 +0900

    Add files via upload

---
+++ project/shaders/light_tex_frag.glsl
@@ -0,0 +1,35 @@
+#version 300 es
+precision mediump float;
+
+uniform sampler2D texImage;
+uniform vec3 matSpec, matAmbi, matEmit;      // Material
+uniform float matSh;
+uniform vec3 srcDiff, srcSpec, srcAmbi;      // Light Source
+uniform vec3 lightDir;      // directional light
+in vec3 fNormal, fView;
+in vec2 fTexCoord;
+
+layout(location = 0) out vec4 fragColor;
+
+void main()
+{
+    // normalization
+    vec3 normal = normalize(fNormal);
+    vec3 view = normalize(fView);
+    vec3 light = normalize(lightDir);
+
+    // diffuse term
+    vec3 matDiff = texture(texImage, fTexCoord).rgb;
+    vec3 diff = max(dot(normal, light), 0.0) * srcDiff * matDiff;
+
+    // specular term
+    //vec3 refl = 2.0 * normal * dot(normal, light) - light;    // Phong Model
+    //vec3 spec = pow(max(dot(refl, view), 0.0), matSh) * srcSpec * matSpec;
+    vec3 halfV = normalize(light + view);                        // Blinn Model
+    vec3 spec = pow(max(dot(normal, halfV), 0.0), matSh) * srcSpec * matSpec;
+
+    // ambient term
+    vec3 ambi = srcAmbi * matAmbi;
+
+    fragColor = vec4(diff + spec + ambi + matEmit, 1.0);
+}
